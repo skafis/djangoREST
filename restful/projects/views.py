5c00ccbeae1edf3746cc0151d01eabec7bf38c19
franck <fkipchumba@intellisoftplus.com>
2016-08-01 09:48:46 +0300

    create views for users and groups

--- restful/projects/views.py
+++ restful/projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from . import UserSerializer, GroupSerializer
 # Create your views here.
+
+class UserViewets (viewsets.ModelViewSet):
+	# Api endpoint allowing users to be viewd or edited
+	queryset = User.objects.all().order_by('-date_joined')
+	serializer_class = UserSerializer
+
+class GroupViewSets(viewsets.ModelViewSet):
+	# API endpoint allowing groups to be viewd or edited
+	queryset = Group.objects.all()
+	serializer_class = GroupSerializer
