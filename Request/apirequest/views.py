e91d359f1d46f641ae99aaa8002ce545930f8c54
franck <fkipchumba@intellisoftplus.com>
2016-08-18 20:16:20 +0300

    add detail view in apirequest view

--- Request/apirequest/views.py
+++ Request/apirequest/views.py
@@ -20,3 +20,28 @@ def project_list(request):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_	BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def project_detail(request, pk):
+	# retreive update or delete project instance
+	try:
+		project =Projects.objects.get(pk=pk)
+	except Projects.DoesNotExist:
+		return Response(status=status.HTTP_404_NOT_FOUND)
+
+	if request.method == 'GET':
+		serializer = ProjectSerializer(project)
+		return Response(serializer.data)
+
+	elif request.method == 'PUT':
+		serializer = ProjectSerializer(project, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	elif request.method == 'DELETE':
+		project.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+
